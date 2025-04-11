6370ff8144cdd5237ee4a387c31511691f08c5b2
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-11 10:42:55 -0500

    metodos para obtener todos los productos de una gama

--- src/main/java/com/isai/demoapigardening_spring/app/repository/ProductRepository.java
+++ src/main/java/com/isai/demoapigardening_spring/app/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.isai.demoapigardening_spring.app.repository;
 
+import com.isai.demoapigardening_spring.app.models.dtos.ProductResponse;
 import com.isai.demoapigardening_spring.app.models.entitys.Product;
+import com.isai.demoapigardening_spring.app.models.entitys.Range;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ProductRepository
         extends JpaRepository<Product, String> {
+    List<Product> findAllByRange(Range range);
 }
