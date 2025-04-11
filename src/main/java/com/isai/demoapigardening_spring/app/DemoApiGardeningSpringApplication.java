b25f3c770b0ba579469efe98bc0dd7e13d474bcc
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-11 08:24:32 -0500

    init

---
+++ src/main/java/com/isai/demoapigardening_spring/app/DemoApiGardeningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demoapigardening_spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiGardeningSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApiGardeningSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isai/demoapigardening_spring/app/DemoApiGardeningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demoapigardening_spring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiGardeningSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
